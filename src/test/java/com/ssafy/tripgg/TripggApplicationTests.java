4cbec20f5c03ee2ca8c2a2aab7f3230a1354b811
심규빈 <skb0516@naver.com>
2024-11-04 08:57:22 +0900

    init: project setting

---
+++ src/test/java/com/ssafy/tripgg/TripggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

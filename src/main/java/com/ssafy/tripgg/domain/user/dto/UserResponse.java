1fe9dd48018f9545d4461f43f69d1be88d1a0024
심규빈 <skb0516@naver.com>
2024-11-06 11:11:12 +0900

    refactor: #9 UserResponse (응답 dto) 활용

---
+++ src/main/java/com/ssafy/tripgg/domain/user/dto/UserResponse.java
@@ -0,0 +1,27 @@
+package com.ssafy.tripgg.domain.user.dto;
+
+import com.ssafy.tripgg.domain.user.entity.User;
+import lombok.*;
+
+@Getter
+@Builder
+@ToString @EqualsAndHashCode
+public class UserResponse {
+    private Long id;
+    private String nickname;
+    private String provider;
+    private String providerId;
+    private String profileImageUrl;
+    private Integer totalPoints;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .provider(user.getProvider().getName())
+                .providerId(user.getProviderId())
+                .profileImageUrl(user.getProfileImageUrl())
+                .totalPoints(user.getTotalPoints())
+                .build();
+    }
+}
--- src/main/java/com/ssafy/tripgg/domain/user/service/UserService.java
+++ src/main/java/com/ssafy/tripgg/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ssafy.tripgg.domain.user.service;
 
+import com.ssafy.tripgg.domain.user.dto.UserResponse;
 import com.ssafy.tripgg.domain.user.entity.User;
 import com.ssafy.tripgg.domain.user.repository.UserRepository;
 import com.ssafy.tripgg.global.error.ErrorCode;
@@ -16,5 +17,11 @@ public class UserService {
 
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    public UserResponse findById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        return UserResponse.from(user);
+    }
     }
 }
